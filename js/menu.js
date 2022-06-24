d7b634503f4b7c870c817769e05dc0eab0bb7460
Maksim Lisovoi <lisovoi123@gmail.com>
2022-06-24 18:23:51 +0300

    added mobile menu and ended main page

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
+  const menuBtnRefClose = document.querySelector('[data-menu-button-close]');
   const mobileMenuRef = document.querySelector('[data-menu]');
 
   menuBtnRef.addEventListener('click', () => {
@@ -8,6 +9,13 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  menuBtnRefClose.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
